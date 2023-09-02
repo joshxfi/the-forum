4a942034b955e8e6c0023f08312588d734f1485b
Josh Daniel <joshxfi.dev@gmail.com>
2023-09-02 16:08:40 +0800

    feat: add message reply

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,17 @@ export default function Home() {
       <Message
         username="joe"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis iure dolor dolorem quos blanditiis quibusdam aut pariatur ex quisquam vitae vel odit ratione labore, consequatur quasi quaerat aliquid! Impedit, sunt?"
-        timestamp="2021-01-01"
+        timestamp="2023-01-01"
         upvoteCount={10}
         replyCount={2}
+      />
+
+      <Message
+        username="johnny"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis iure dolor dolorem quos blanditiis quibusdam aut pariatur ex quisquam vitae vel odit ratione labore, consequatur quasi quaerat aliquid! Impedit, sunt?"
+        timestamp="2023-02-04"
+        upvoteCount={7}
+        replyCount={4}
       />
     </section>
   );
--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,23 +1,66 @@
+"use client";
+import React, { useState } from "react";
 import { Icons } from "./icons";
 
 type Props = {
   username: string;
   content: string;
   timestamp: string;
-  upvoteCount?: number;
-  replyCount?: number;
+  upvoteCount: number;
+  replyCount: number;
 };
 
-export function Message({
+export function Message({ ...rest }: Props) {
+  const [showReplies, setShowReplies] = useState(false);
+
+  return (
+    <div className="pb-8">
+      <Post {...rest} type="origin" setShowReplies={setShowReplies} />
+
+      {showReplies && (
+        <div>
+          <Post
+            username="jane04"
+            content="that's amazing! 🚀"
+            timestamp="2023-08-11"
+            upvoteCount={2}
+            replyCount={0}
+            type="reply"
+          />
+
+          <Post
+            username="user_091"
+            content="I'm so proud of you!"
+            timestamp="2023-08-11"
+            upvoteCount={4}
+            replyCount={0}
+            type="reply"
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+const Post = ({
   username,
   content,
   timestamp,
   upvoteCount,
   replyCount,
-}: Props) {
+  type = "origin",
+  setShowReplies,
+}: Props & {
+  type: "origin" | "reply";
+  setShowReplies?: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
   return (
     <div className="border-b border-muted pb-8">
-      <div className="text-sm container max-w-screen-sm">
+      <div
+        className={`text-sm container max-w-screen-sm ${
+          type === "reply" && "pl-16 pt-8"
+        }`}
+      >
         <div className="flex gap-x-2 mb-2">
           <h2 className="font-semibold">{username}</h2>
           <p className="text-muted-foreground">{timestamp}</p>
@@ -25,7 +68,7 @@ export function Message({
 
         <p>{content}</p>
 
-        <div className="mt-2 flex gap-x-2">
+        <div className="mt-2 flex gap-x-2 items-center">
           <div className="flex gap-x-1 items-center">
             <button>
               <Icons.arrowUp className="w-6 h-6" />
@@ -34,15 +77,17 @@ export function Message({
             <p className="text-muted-foreground">{upvoteCount}</p>
           </div>
 
-          <div className="flex gap-x-1 items-center">
-            <button>
-              <Icons.reply className="w-6 h-6" />
-            </button>
+          {setShowReplies && type === "origin" && (
+            <div className="flex gap-x-1 items-center">
+              <button type="button" onClick={() => setShowReplies((p) => !p)}>
+                <Icons.reply className="w-6 h-6" />
+              </button>
 
-            <p className="text-muted-foreground">{replyCount}</p>
-          </div>
+              <p className="text-muted-foreground">{replyCount}</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
-}
+};
