cae9477ef9c8b268a7e0e50dc6fab6f03893b233
Josh Daniel <joshxfi.dev@gmail.com>
2023-09-03 19:35:24 +0800

    feat: add progress bar on top

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,9 @@
-import { Navbar } from "@/components/navbar";
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import NextTopLoader from 'nextjs-toploader';
+
+import "./globals.css";
+import { Navbar } from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NextTopLoader showSpinner={false} />
         <Navbar />
         {children}
       </body>
