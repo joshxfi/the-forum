b2731141b0984dddb98bd382f2be8671a67409b6
Josh Daniel <joshxfi.dev@gmail.com>
2023-09-03 13:49:36 +0800

    fix: add padding bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Message } from "@/components/message";
 
 export default function Home() {
   return (
-    <section>
+    <section className="pb-24">
       <Message
         username="joe"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis iure dolor dolorem quos blanditiis quibusdam aut pariatur ex quisquam vitae vel odit ratione labore, consequatur quasi quaerat aliquid! Impedit, sunt?"
