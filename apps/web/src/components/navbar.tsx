24aae0772edf6fc0aa16e4777436140d97ec862f
Josh Daniel <joshxfi.dev@gmail.com>
2023-09-10 19:40:07 +0800

    chore: update navbar  logo

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,10 +1,9 @@
 export function Navbar() {
   return (
     <nav className="container max-w-screen-sm mb-8 pt-12">
-      <h1 className="uppercase text-sm">The Forum</h1>
-      <p className="text-muted-foreground text-xs">
-        an open space for bcd universities
-      </p>
+      <h1 className="uppercase text-sm text-muted-foreground">
+        The<span className="font-bold text-white">Forum</span>
+      </h1>
     </nav>
   );
 }
