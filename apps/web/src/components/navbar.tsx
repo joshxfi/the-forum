bd103acb378333ab83fff0a7c6fc52926c6fd2c1
Josh Daniel <joshxfi.dev@gmail.com>
2023-09-13 08:21:17 +0800

    chore(temp): add signout button

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { signOut } from "next-auth/react";
+
 export function Navbar() {
   return (
-    <nav className="container max-w-screen-sm mb-16 pt-12">
+    <nav className="container max-w-screen-sm mb-16 pt-12 flex justify-between items-center">
       <h1 className="uppercase text-sm text-muted-foreground">
         The<span className="font-bold text-white">Forum</span>
       </h1>
+
+      <Button type="button" onClick={() => signOut()}>
+        Logout
+      </Button>
     </nav>
   );
 }
