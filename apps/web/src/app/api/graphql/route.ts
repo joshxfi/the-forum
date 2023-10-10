edfadd1d1ffbd83b34669389d5739b01a86e360f
Josh Daniel <joshxfi.dev@gmail.com>
2023-10-10 19:27:53 +0800

    chore: add type in handler

--- apps/web/src/app/api/graphql/route.ts
+++ apps/web/src/app/api/graphql/route.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import { NextRequest } from "next/server";
 import { buildSchema } from "type-graphql";
 import { ApolloServer } from "@apollo/server";
 import { getServerSession } from "next-auth/next";
@@ -17,7 +18,7 @@ const server = new ApolloServer({
   schema,
 });
 
-const handler = startServerAndCreateNextHandler(server, {
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
   context: async () => {
     const session = await getServerSession(authOptions);
     const id = session?.user?.id;
