d0e8d487e9c7fddd957f9cedb5a83fbfd5c9b9d2
Josh Daniel <joshxfi.dev@gmail.com>
2023-10-22 16:35:27 +0800

    fix: add graphql endpoint to cors

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: process.env.NEXTAUTH_URL,
+            value: process.env.NEXT_PUBLIC_GRAPHQL_URL,
           },
           {
             key: "Access-Control-Allow-Methods",
