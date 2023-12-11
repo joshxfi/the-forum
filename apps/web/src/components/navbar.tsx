87dc67b361345417671d8b36632269f7bc63b4ef
Josh Daniel <joshxfi.dev@gmail.com>
2023-12-11 19:50:40 +0800

    chore: add link to homepage

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import Link from "next/link";
+
 export function Navbar() {
   return (
     <nav className="container max-w-screen-sm mb-16 pt-12">
-      <h1 className="uppercase text-sm text-muted-foreground">
+      <Link href="/" className="uppercase text-sm text-muted-foreground">
         The<span className="font-bold text-white">Forum</span>
-      </h1>
+      </Link>
     </nav>
   );
 }
